79136b095f9fb5fe713a454daa882242861804fe
Thomas Hoenselaars <thomas@angistudio.com>
2021-03-09 12:55:11 +0100

    basic setup

---
+++ code.ts
@@ -0,0 +1,29 @@
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, {
+  width: 350,
+  height: 540,
+});
+
+figma.ui.onmessage = async (msg: MessageEvent) => {
+  await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
+  const oldStyles = figma.getLocalPaintStyles().map((style) => style);
+
+  if (msg.type === 'create-theme') {
+    console.log(oldStyles)
+  }
+
+  switch (msg.type) {
+    case 'create-theme':
+      console.log(msg.type)
+      break;
+
+    default:
+      break;
+  }
+
+  // Make sure to close the plugin when you're done.
+  figma.closePlugin();
+};
+
+
+
---
+++ js/createTheme/createTheme.js
@@ -0,0 +1,3 @@
+export const createTheme = () => {
+    console.log('creating theme');
+};
