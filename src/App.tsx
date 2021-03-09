fcbbdd1bd1bc803d0ed35f6ac335daf059351cf6
Thomas Hoenselaars <thomas@angistudio.com>
2021-03-09 14:29:32 +0100

    fixed typing issue and scss issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { sendMessage } from './helpers/sendMessage';
+import './scss/main.scss';
 
 export const App: React.FC = () => {
+  const [theme, setTheme] = React.useState('');
+
+  const handleTextArea = (e: any) => {
+    setTheme(e.target.value);
+  }
+
+  const handleSubmit = () => sendMessage('createTheme', theme);
+
+  useEffect(() => {
+    sendMessage('pluginLoaded', null);
+  }, [])
 
   return (
     <div className="app">
-      I am the app
-      <button onClick={() => sendMessage('create-theme', null)}>send msg</button>
+      <h2>TokensOfTruth</h2>
+      <textarea name="text-input" id="text-input" cols={30} rows={10} placeholder="Paste Theme JSON..." value={theme} onChange={handleTextArea}></textarea>
+      <button onClick={handleSubmit}>send msg</button>
     </div>
   );
 }
--- src/code.ts
+++ src/code.ts
@@ -1,20 +1,22 @@
-// This shows the HTML page in "ui.html".
+import PluginAPI from '@figma/plugin-typings';
+
 figma.showUI(__html__, {
   width: 350,
   height: 540,
 });
 
 figma.ui.onmessage = async (msg: MessageEvent) => {
-  await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
   const oldStyles = figma.getLocalPaintStyles().map((style) => style);
 
-  if (msg.type === 'create-theme') {
-    console.log(oldStyles)
-  }
-
   switch (msg.type) {
-    case 'create-theme':
+    case 'createTheme':
       console.log(msg.type)
+      console.log(msg.data)
+      break;
+
+    case 'pluginLoaded':
+      console.log('loaded')
+      await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
       break;
 
     default:
